420961ebfb96518e69ab47ce62670ebfaa6ed7ae
afsana-haque <afsana.haque3301@gmail.com>
2024-05-22 17:49:49 +0600

    basic set up

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import app from './app';
+import mongoose from 'mongoose';
+import config from './app/config';
+
+
+
+
+async function main() {
+ 
+  try {
+    await mongoose.connect(config.database_url as string);
+
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+main();
+
+//Tu9B2Ar7742G3rot
